0b8bfdf2c2d9cc78a0a91b4f9dd112df2056019c
Rostyslav` <hrostyslav@ukr.net`>
2026-05-13 13:48:56 +0300

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,16 +21,18 @@ const food = {
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  if (Object.keys(data).length === 0) return;
+  if (Object.keys(data).length === 0) {
+    return;
+  }
 
   const ul = document.createElement('ul');
 
-  for(const key in data) {
+  for (const key in data) {
     const li = document.createElement('li');
     li.textContent = key;
 
     if (Object.keys(data[key]).length > 0) {
-      createTree(li, data[key])
+      createTree(li, data[key]);
     }
 
     ul.append(li);
